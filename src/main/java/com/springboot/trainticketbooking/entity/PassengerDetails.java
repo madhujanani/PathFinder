ee9eb4ce52031f8c0a8c0c3127cfccba99b97f64
Tom Spitz <tjspitz1986@gmail.com>
2024-01-30 15:45:06 -0600

    Refactor the PNR so it is id/primary key in the entity It will generate the random UUID for us Add Javadoc to the repository interface

--- src/main/java/com/springboot/trainticketbooking/entity/PassengerDetails.java
+++ src/main/java/com/springboot/trainticketbooking/entity/PassengerDetails.java
@@ -3,17 +3,19 @@ package com.springboot.trainticketbooking.entity;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Random;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @author madhu
- *
+ * @author Madhu, Tom
+ * 
  */
 
 @NoArgsConstructor
@@ -22,24 +24,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PassengerDetails {
 
-	private String passengerName;
-	private String fromStation;
-	private String toStation;
+    @Id
+    private String pnr = makeId();
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-	private Date dateOfTravel;
+    private String passengerName;
+    private String fromStation;
+    private String toStation;
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-	private Date updatedTicketDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date dateOfTravel;
 
-	private String trainNo;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date updatedTicketDate;
 
-	private Random pnr = new Random();
+    private String trainNo;
 
-	private LocalDate dateOfBooking = java.time.LocalDate.now();
+    private LocalDate dateOfBooking = java.time.LocalDate.now();
 
-	private String bookingStatus = new Random().nextBoolean() ? "confirmed" : "waiting";
+    private String bookingStatus = new Random().nextBoolean() ? "confirmed" : "waiting";
 
-	private double ticketFair = Math.floor(Math.random() * 31) + 50;
+    private double ticketFair = Math.floor(Math.random() * 31) + 50;
+
+    private String makeId() {
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase().substring(0, 7);
+    }
 
 }
