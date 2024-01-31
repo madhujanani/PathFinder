1056fbd7d9b3eea08aea7c0f83e49c5c5c1c2c12
Tom Spitz <tjspitz1986@gmail.com>
2024-01-30 19:41:54 -0600

    Fixed package naming convention so Spring Boot will Boot its Springs

--- src/main/java/com/springboot/trainticketbooking/TrainTicketBookingApplication.java
+++ src/main/java/com/springboot/trainticketbooking/TrainTicketBookingApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.trainticketbooking.TrainTicketBooking;
+package com.springboot.trainticketbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
