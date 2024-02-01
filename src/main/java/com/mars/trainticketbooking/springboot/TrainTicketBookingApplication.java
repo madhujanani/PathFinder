804800fc2e262db6f5ec2e5c1b2905895cab1997
abc@mail.com <abc@mail.com>
2024-02-01 05:10:36 -0600

    'commit-1'

--- src/main/java/com/mars/trainticketbooking/springboot/TrainTicketBookingApplication.java
+++ src/main/java/com/mars/trainticketbooking/springboot/TrainTicketBookingApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.trainticketbooking;
+package com.mars.trainticketbooking.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
