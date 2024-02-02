b08229ba73c2a9be9bf9dcf0b097df6c8038562e
Saibhavithra <saibhavithra@gmail.com>
2024-02-02 11:04:41 -0600

    ss

--- src/main/java/com/springboot/trainticketbooking/service/TicketServiceImpl.java
+++ src/main/java/com/springboot/trainticketbooking/service/TicketServiceImpl.java
@@ -1,8 +1,6 @@
 
+package com.springboot.trainticketbooking.service;
 
-
-	package com.springboot.trainticketbooking.service;
-	 
 import java.util.Random;
 import java.util.UUID;
 
@@ -13,61 +11,58 @@ import com.springboot.trainingticketbooking.repository.TicketRepository;
 import com.springboot.trainticketbooking.constant.BookingStatus;
 import com.springboot.trainticketbooking.entity.Ticket;
 import com.springboot.trainticketbooking.model.PassengerDetails;
-	 
-	/**
-	* @author Madhu
-	*/
-	 
-	@Service
-	public class TicketServiceImpl implements TicketService {
-	 
-	   // @Autowired
-	 private TicketRepository repository;
-	 
-	    @Autowired
-	    public TicketServiceImpl(TicketRepository repository) {
+
+/**
+ * @author Madhu
+ */
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+	// @Autowired
+	private TicketRepository repository;
+
+	@Autowired
+	public TicketServiceImpl(TicketRepository repository) {
 		super();
 		this.repository = repository;
 	}
 
+	/**
+	 * @author Sai
+	 */
 
-		/**
-	     * @author Sai
-	     */
+	@Override
+	public Ticket bookTicket(PassengerDetails passengerdetails) {
+		// generate the random booking status to the train ticket
+		Random random = new Random();
+		int index = random.nextInt(BookingStatus.values().length);
+		BookingStatus randomStatus = BookingStatus.values()[index];
+		// generate a PNR ticket using Random, UUID, or external API
+		UUID uuid = UUID.randomUUID();
+		String pnr = uuid.toString().replace("-", "").substring(0, 7);
+		// generate a random TicketFair
+		int randomNumber = (int) (Math.random() * 9999);
+		if (randomNumber <= 1000) {
+			randomNumber = randomNumber + 1000;
+		}
 
-	 
-		
-	    	@Override
-			public Ticket bookTicket(PassengerDetails passengerdetails) {
-		             //generate the random booking status to the train ticket   
-	                   Random random = new Random();
-	                   int index = random.nextInt(BookingStatus.values().length);
-	                   BookingStatus randomStatus = BookingStatus.values()[index];
-	                 //generate a PNR ticket using Random, UUID, or external API    
-	                     UUID uuid = UUID.randomUUID();
-	                     String pnr = uuid.toString().replace("-", "").substring(0, 7);
-	                //generate a random TicketFair
-	                     int randomNumber = ( int )( Math. random() * 9999 ); 
-	           		     if( randomNumber <= 1000 ) { randomNumber = randomNumber + 1000;}
-	 
-	     
-			        Ticket ticket = new Ticket();
-			        ticket.setPassengerName(passengerdetails.getPassengerName());
-			        ticket.setToStation(passengerdetails.getToStation());
-			        ticket.setFromStation(passengerdetails.getFromStation());
-			        ticket.setDateOfTravel(passengerdetails.getDateOfTravel());
-			        ticket.setTrainNo(passengerdetails.getTrainNo());
-			        ticket.setPnr(pnr);
-			        ticket.setDateOfBooking (java.time.LocalDate.now());
-			        ticket.setBookingStatus (randomStatus);
-			        ticket.setTicketFare(randomNumber);
-	 
-			        //save the ticket to the database using the ticketRepository
-			        repository.save(ticket);
-	 
-			        // return the ticket
-			        return ticket;
-			    }
-	 
+		Ticket ticket = new Ticket();
+		ticket.setPassengerName(passengerdetails.getPassengerName());
+		ticket.setToStation(passengerdetails.getToStation());
+		ticket.setFromStation(passengerdetails.getFromStation());
+		ticket.setDateOfTravel(passengerdetails.getDateOfTravel());
+		ticket.setTrainNo(passengerdetails.getTrainNo());
+		ticket.setPnr(pnr);
+		ticket.setDateOfBooking(java.time.LocalDate.now());
+		ticket.setBookingStatus(randomStatus);
+		ticket.setTicketFare(randomNumber);
+
+		// save the ticket to the database using the ticketRepository
+		repository.save(ticket);
+
+		// return the ticket
+		return ticket;
+	}
 
 }
