90d90861712ccf296087028f7377055a29be10f7
Tom Spitz <tjspitz1986@gmail.com>
2024-02-12 17:40:00 -0600

    comment out GET method since it does not exist in service layer yet

--- src/main/java/com/mars/trainticketbooking/springboot/controller/TicketController.java
+++ src/main/java/com/mars/trainticketbooking/springboot/controller/TicketController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -84,14 +85,14 @@ public class TicketController {
 	 * @param pnr
 	 * @return ResponseEntity
 	 */
-	@GetMapping("/search/{pnr}")
-	public ResponseEntity<Ticket> getTicket(@RequestBody Ticket ticket, @PathVariable(name = "pnr") String pnr)
-
-	{
-		Ticket searchedTicket = service.getTicketByPnr(ticket, pnr);
-		return new ResponseEntity<>(searchedTicket, HttpStatus.OK);
-
-	}
+//	@GetMapping("/search/{pnr}")
+//	public ResponseEntity<Ticket> getTicket(@RequestBody Ticket ticket, @PathVariable(name = "pnr") String pnr)
+//
+//	{
+//		Ticket searchedTicket = service.getTicketByPnr(ticket, pnr);
+//		return new ResponseEntity<>(searchedTicket, HttpStatus.OK);
+//
+//	}
 
 
 }
