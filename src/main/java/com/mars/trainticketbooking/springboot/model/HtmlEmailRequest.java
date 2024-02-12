6c66af583c26fae53606e98a572180d08a9b1c2f
Tom Spitz <tjspitz1986@gmail.com>
2024-02-12 17:41:06 -0600

    implement Simple and Html email functionality

---
+++ src/main/java/com/mars/trainticketbooking/springboot/model/HtmlEmailRequest.java
@@ -0,0 +1,22 @@
+package com.mars.trainticketbooking.springboot.model;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tjspitz
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+public class HtmlEmailRequest extends SimpleEmailRequest {
+	
+	private String pnr;
+	private LocalDate travelDate;
+	private double fare;
+}
---
+++ src/main/java/com/mars/trainticketbooking/springboot/model/SimpleEmailRequest.java
@@ -0,0 +1,18 @@
+package com.mars.trainticketbooking.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tjspitz
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleEmailRequest {
+
+	private String recipient;
+	private String subject;
+	private String body;
+}
